ae071ea26ee154d02c0f1529458ee92dcf3135b5
obi-1-konobe <obi1konobe@gmail.com>
2020-05-08 22:50:34 +0300

    prepare feature extraction for bow

--- modules/features_extractor.py
+++ modules/features_extractor.py
@@ -10,14 +10,14 @@ class Extractor:
     def run(self):
         path = '../data/fma_small/'
         dirs = os.listdir(path)
-        essentia_listdir = os.listdir('../data/essentia/')
+        essentia_listdir = os.listdir('../data/essentia_bow/')
         for some_dir in dirs:
             list_dir = os.listdir(f'{path}{some_dir}/')
             for track in list_dir:
                 if (track[:-4] + '.json') in essentia_listdir:
                     continue
                 try:
-                    self.extract_features_all(f'{path}{some_dir}/', track)
+                    # self.extract_features_all(f'{path}{some_dir}/', track)
                     self.extract_features_intervals(f'{path}{some_dir}/', track)
                 except Exception:
                     with open('../data/essentia_errors.txt', 'a+') as f:
@@ -26,9 +26,9 @@ class Extractor:
 
     @staticmethod
     def extract_features_intervals(path, track_name):
-        intervals = list(range(4, 33, 4))
+        intervals = list(range(1, 31))
         for i in intervals:
-            start = i - 4
+            start = i - 1
             end = i
             features, features_frames = es.MusicExtractor(
                 lowlevelStats=['mean', 'stdev', 'min', 'max'],
@@ -37,7 +37,7 @@ class Extractor:
                 startTime=start,
                 endTime=end
             )(f'{path}{track_name}')
-            save_file = f'../data/essentia/{track_name[:-4]}_{int(i/4)}.json'
+            save_file = f'../data/essentia_bow/{track_name[:-4]}_{int(i/4)}.json'
             es.YamlOutput(filename=save_file, format="json")(features)
 
     @staticmethod
